c4c8423125fbdbc8187dd44d7312e4b20337ccd0
kardoszc <kardos_zc@outlook.com>
2016-05-17 15:19:55 +0800

    update

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class Point(object):
         self.y = y
         self.valid = True
     def __str__(self):
-        print "%s,%s" % (x,y)
+        return "%s,%s" % (self.x, self.y)
 
 class Line(object):   
     def __init__(self, start, end):
@@ -30,8 +30,8 @@ class Line(object):
     
     #两直线距离，相交直线为0
     def distance_to_line(self, other_line):
-        if abs(self.angle - other_line.angle) > 0.01 :
-            return 0
+        # if abs(self.angle - other_line.angle) > 0.01 :
+        #     return 0
         #y = kx + b1 & y = kx + b2 两平行直线的k 
         k = math.tan(self.angle)/2 + math.tan(other_line.angle)/2
         
@@ -41,13 +41,13 @@ class Line(object):
         return abs(molecule / denominator)
 
     def __str__(self):
-        print "%s,%s" % (start,end)
+        return "line from %s to %s" % (self.start, self.end)
     
 class Speed(object):
     """docstring for Speed"""
     def __init__(self,filename):
-        self.MinTrackWidth = 1
-        self.MaxTrackWidth = 10
+        self.MinTrackWidth = 5
+        self.MaxTrackWidth = 20
 
     	start = time.time()
         self.dxf = dxfgrabber.readfile(filename)
@@ -55,7 +55,7 @@ class Speed(object):
         self.lines = self.get_line()
         print "get_line" , time.time() - start
 
-        self.draw_line(self.get_track())
+        self.draw_line(self.lines)
     
     def find_layer_by_name(self,name):
         for layer in self.dxf.layers:
@@ -64,13 +64,22 @@ class Speed(object):
     
     def get_line(self):
         lines = []
-        for entity in self.dxf.entities:
-            if self.find_layer_by_name(entity.layer).on and entity.dxftype == 'LINE':
+        entities_on = [entity for entity in self.dxf.entities if self.find_layer_by_name(entity.layer).on ]
+
+        for entity in entities_on:
+            if entity.dxftype == 'LINE' :
                 start = Point(entity.start[0], entity.start[1])
                 end = Point(entity.end[0], entity.end[1])
                 line = Line(start, end)
                 lines.append(line)
-            elif self.find_layer_by_name(entity.layer).on and entity.dxftype == 'POLYLINE':
+            elif entity.dxftype == 'POLYLINE' :
+                pl_points = entity.points
+                for i in range(len(pl_points) - 1 ):
+                    start = Point(pl_points[i-1][0], pl_points[i-1][1])
+                    end = Point(pl_points[i][0], pl_points[i][1])
+                    line = Line(start, end)
+                    lines.append(line)
+            elif entity.dxftype == 'LWPOLYLINE' :
                 pl_points = entity.points
                 for i in range(len(pl_points) - 1 ):
                     start = Point(pl_points[i-1][0], pl_points[i-1][1])
@@ -85,12 +94,13 @@ class Speed(object):
             if not line1.valid:
                 continue
             for line2 in self.lines:
-                if line1 == line2 or not line2.valid :
-                    continue
-                if abs(line1.angle - line2.angle) > 0.01:
-                    continue
-                if line1.distance_to_line(line2) < self.MinTrackWidth or line1.distance_to_line(line2) > self.MaxTrackWidth:
-                	continue
+
+                # if line1 == line2 or not line2.valid :
+                #     continue
+                # if abs(line1.angle - line2.angle) > 0.01:
+                #     continue
+                # if line1.distance_to_line(line2) < self.MinTrackWidth or line1.distance_to_line(line2) > self.MaxTrackWidth:
+                #     continue
                 line1.valid = False
                 line2.valid = False
                 track.extend([line1,line2])
@@ -99,7 +109,7 @@ class Speed(object):
     def draw_line(self, lines):
         all_points = [l.start for l in lines] + [l.end for l in lines]
         all_x,all_y = [p.x for p in all_points] , [p.y for p in all_points]
-        size = [int(max(all_x)-min(all_x)),int(max(all_y)-min(all_y))]
+        size = [int(max(all_x)-min(all_x))+200,int(max(all_y)-min(all_y))+200]
         im = Image.new('RGB', size, (255,255,255))
         draw = ImageDraw.Draw(im)
         center = Point(sum(all_x)/len(all_x), sum(all_y)/len(all_y))
